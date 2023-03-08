0412af4a0021a02f675c2e282d4c673fe6dc9229
fdsssawe <sashasvl27012004@gmail.com>
2023-03-08 15:42:02 +0200

    path changed

--- router/index.js
+++ router/index.js
@@ -11,3 +11,6 @@ router.post("/logout" , userController.logout)
 router.get("/activate/:link" , userController.activate)
 router.get("/refresh" , userController.refresh)
 router.get("/users" , authMiddleware , userController.getUsers)
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+  });
