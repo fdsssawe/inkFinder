841d5e015c2029fa281f7aa093e338b566b87dc1
Olexander Zhovanyk <mindesync@gmail.com>
2023-06-09 19:37:58 +0300

    Hotfix

--- client/csletmelearn/src/components/Header.jsx
+++ client/csletmelearn/src/components/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
                         {({ active }) => (
                     <button 
                     onClick={()=>{  
-                        navigate("/api-docs")
+                        navigate("/api-docs/")
                     }}
                     class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 text-gray-400">
                         Swagger
