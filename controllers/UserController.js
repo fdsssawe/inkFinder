347c3305de34579abec7cf1eddaf360a99a9b8d6
Olexander Zhovanyk <mindesync@gmail.com>
2024-05-02 11:57:26 +0300

    Hotfix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -61,7 +61,6 @@ export class UserController {
       const userData = await userServiceContainer.resolve("userService").refresh(refreshToken);
       res.cookie('refreshToken', userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
         sameSite: 'None', // set SameSite to None
         secure: true, // set Secure to true
     });
